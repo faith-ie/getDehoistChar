fc5d851b991051906b188e7d7e28ebfe19f02b6e
Daniel Viola <daniel.viola001@mymdc.net>
2023-07-22 13:19:37 -0400

    format

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const fs = require('fs')
-fs.writeFile('./DehoistChar.txt', String.fromCharCode((0x17B5)), async function (err) {
-  if (err) {
-    console.log(err)
+const fs = require('fs');
+fs.writeFile(
+  './DehoistChar.txt',
+  String.fromCharCode(0x17b5),
+  async function (err) {
+    if (err) {
+      console.log(err);
+    }
   }
-})
+);
